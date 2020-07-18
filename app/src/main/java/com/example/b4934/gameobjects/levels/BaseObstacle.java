73d553181dcdfa8f06c9a2fb937357dfad535ea9
Moustafa Mohamed <Moustafa @ M0D4>
2020-07-19 01:17:28 +0200

    BaseObstacle

---
+++ app/src/main/java/com/example/b4934/gameobjects/levels/BaseObstacle.java
@@ -0,0 +1,18 @@
+package com.example.b4934.gameobjects.levels;
+
+import android.graphics.Bitmap;
+
+import com.example.b4934.core.StaticCharacterSprite;
+
+public class BaseObstacle extends StaticCharacterSprite {
+
+
+    public BaseObstacle(Bitmap bmp) {
+        super(bmp);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
