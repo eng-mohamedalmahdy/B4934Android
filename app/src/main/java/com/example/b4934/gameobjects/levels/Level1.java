fbda60063b9201a9ea3d050975dc6b9ca21d1b9d
Moustafa Mohamed <Moustafa @ M0D4>
2020-07-19 01:10:10 +0200

    App has a "crash" on me.

--- app/src/main/java/com/example/b4934/gameobjects/levels/Level1.java
+++ app/src/main/java/com/example/b4934/gameobjects/levels/Level1.java
@@ -7,15 +7,17 @@ import com.example.b4934.core.Level;
 import com.example.b4934.gameobjects.Player;
 import com.example.b4934.utils.SpritesFactory;
 
-public class Level1 {
+public class Level1{
     private Level level;
-
+    private BaseObstacle ob1;
     public Level1(Context context) {
-
+        ob1 = new BaseObstacle(SpritesFactory.getObstacle1Sprite(context));
+        ob1.setX(50);
+        ob1.setY(50);
         Bitmap background = SpritesFactory.getLevel1BackgroundSprite(context);
         Player player = new Player(SpritesFactory.getOwlSprites(context));
-        level = new Level(context, player, background);
-
+        level = new Level(context, player);
+        level.addObject(ob1);
     }
 
     public Level getLevel() {
