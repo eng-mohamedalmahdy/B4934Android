92a9fefb3632be2042d33d8b8f9113c960527496
mahdy1234 <engmohamedalmahdy@gmail.com>
2020-07-18 19:59:43 +0300

    kotlin replaced

---
+++ app/src/main/java/com/example/b4934/ui/activities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.b4934.ui.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.b4934.R;
+import com.example.b4934.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+    private Button play;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initComponents();
+
+    }
+
+    private void initComponents() {
+        play = binding.play;
+        play.setOnClickListener((cl) -> {
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
+        });
+    }
+}
